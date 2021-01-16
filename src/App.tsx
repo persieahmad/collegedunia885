48bb92582ec535b8d90ffcd1f969a9effa6dc0eb
Ahmad Sadiq <ahmadsadiq885@gmail.com>
2021-01-17 00:36:13 +0530

    card ui half done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { colleges } from "./colleges.json";
+import photo from "./college.jpg";
+import StarRatings from "react-star-ratings";
 
 function App() {
+  const isMobile: boolean = window.innerWidth <= 500;
+  const all = [colleges[0], colleges[1]];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p
+        style={{
+          marginLeft: isMobile ? 12 : 60,
+          fontSize: isMobile ? 16 : 30,
+          fontFamily: "bold",
+        }}
+      >
+        Colleges in North India
+      </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {all.map((data, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                margin: 10,
+              }}
+            >
+              <img
+                alt={data.college_name}
+                src={photo}
+                style={{
+                  width: "100%",
+                  maxWidth: "700px",
+                  height: "auto",
+                  filter: "brightness(30%)",
+                }}
+              />
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <p style={{ fontSize: "1.2vw" }}>{data.college_name}</p>
+                <div
+                  style={{
+                    marginLeft: isMobile ? 4 : 10,
+                    marginTop: isMobile ? -9 : 18,
+                  }}
+                >
+                  <StarRatings
+                    rating={data.rating}
+                    starRatedColor="black"
+                    numberOfStars={5}
+                    name="rating"
+                    starDimension={isMobile ? "4" : "12"}
+                    starSpacing={"1"}
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginLeft: isMobile ? 36 : 255,
+                    marginTop: isMobile ? -6 : 6,
+                  }}
+                >
+                  <p style={{ fontSize: isMobile ? 8 : 14 }}>
+                    <s>₹{data.original_fees}</s>
+                  </p>
+                  <div>
+                    <p
+                      style={{
+                        color: "#ffffff",
+                        fontSize: isMobile ? 8 : 14,
+                        fontFamily: "Raleway,Helvetica",
+                        backgroundColor: "#E80707",
+                        marginLeft: isMobile ? 4 : 6,
+                        marginTop: isMobile ? 10 : 16,
+                      }}
+                    >
+                      -{data.discount}%
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
